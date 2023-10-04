bed54cbcbb58153d64ad83bad05f2c5c1502b202
Gogulan12 <gogulanravi12@gmail.com>
2023-10-04 15:43:49 -0400

    updated home page hero and navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import "./App.css";
 import logo from "./logo.svg";
+import { BrowserRouter, Route, Switch, NavLink } from "react-router-dom";
+
+// page components
+import Home from "./Pages/Home";
+import Clio from "./Pages/Clio";
+import About from "./Pages/About";
+import Contact from "./Pages/Contact";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-        <img src={logo} alt="" />
-      </header>
+      <BrowserRouter>
+        {/* <header className="App-header">
+          <img src={logo} alt="" />
+        </header> */}
+        <nav>
+          <NavLink exact to="/">
+            <img src={logo} alt="" />
+          </NavLink>
+          {/* <a href="/">Home</a>
+          <a href="/clio">Clio OCR</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a> */}
+          <div>
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+            <NavLink to="/clio">Clio OCR</NavLink>
+            <NavLink to="/about">About</NavLink>
+            <NavLink to="/contact">Contact</NavLink>
+          </div>
+        </nav>
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/clio">
+            <Clio />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,12 @@
 import React from "react";
+import "./Home.css";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="mainHeroContainer">
+      <div className="mainHero">
+        <h1>Automate everyday legal workflows inside Clio.</h1>
+      </div>
+    </div>
+  );
 }

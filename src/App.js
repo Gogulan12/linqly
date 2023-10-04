5261146d122ebf74c696fdbdabff1aeed9c5ab4c
Gogulan12 <gogulanravi12@gmail.com>
2023-10-04 13:39:25 -0400

    updated favicon and home page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
@@ -16,7 +15,8 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
+        <img src={logo} alt="" />
       </header>
     </div>
   );

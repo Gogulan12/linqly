9bedd9e8790cf0d5a95117127b5786cd2dd9b068
Gogulan12 <gogulanravi12@gmail.com>
2023-10-07 14:42:04 -0400

    updated home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,83 @@
 import React from "react";
 import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPiggyBank,
+  faBriefcase,
+  faShield,
+  faArrowUpRightDots,
+} from "@fortawesome/free-solid-svg-icons";
 
 export default function Home() {
   return (
-    <div className="mainHeroContainer">
-      <div className="mainHero">
-        <h1>Automate everyday legal workflows inside Clio.</h1>
+    <div>
+      <div className="mainHeroContainer">
+        <div className="mainHero">
+          <h1>
+            Automate everyday <br />
+            legal workflows <br />
+            inside Clio.
+          </h1>
+          <h4>Transforming Legal Documents with OCR Expertise</h4>
+          <div className="form-container">
+            <form action="" method="">
+              <label for="email"></label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                placeholder="Enter your email"
+                required
+              />
+              <input type="submit" value="Submit" id="submit" />
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="mainContent">
+        <h2>
+          Focus on your clients. <br />
+          Automate the rest.
+        </h2>
+        <div className="listofBenefits">
+          <ul>
+            <li>
+              <FontAwesomeIcon icon={faPiggyBank} className="money icon" />
+              <h3>Save time and money</h3>
+              <p>
+                Do away with busy work and focus more on giving exceptional
+                client experiences.
+              </p>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faBriefcase} className="work icon" />
+              <h3>Better client experience</h3>
+              <p>
+                Move your cases through faster by having automations do your
+                work and client communications.
+              </p>
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faShield} className="shield icon" />
+              <h3>Eliminate human error</h3>
+              <p>
+                No more human errors in data entry or client communications. Let
+                the machines handle that.
+              </p>
+            </li>
+            <li>
+              <FontAwesomeIcon
+                icon={faArrowUpRightDots}
+                className="growth icon"
+              />
+              <h3>Grow your profits</h3>
+              <p>
+                Grow your revenue but not your expenses. LegalMate allows you to
+                do more cases with less time.
+              </p>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
